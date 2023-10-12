c3572280311f49c5aef7ccdfe8e4942508e5370b
Kerry Johnson <m263156@usna.edu>
2023-10-12 13:31:08 -0400

    updated game.py
    added blue screen and brown ground

--- game.py
+++ game.py
@@ -1,2 +1,17 @@
-print("Hello world")
-print("new message")
+import pygame
+import time
+
+print(f"the quit event is {pygame.QUIT}")
+pygame.init()
+
+screen = pygame.display.set_mode((400, 400))
+pygame.display.set_caption("Chomp!")
+screen.fill((100,100,150))
+pygame.draw.rect(screen, (100,25,0), (0, 380, 400, 400))
+pygame.draw.rect(screen, (100,25,0), (0, 200, 200, 200))
+pygame.display.flip()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            print("nice try")
