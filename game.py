7681e5c282119dcf81f252a6a7f1e311b2f0ff9e
Kerry Johnson <m263156@usna.edu>
2023-10-13 09:10:49 -0400

    Fish
    Updated to add a green square as a fish

--- game.py
+++ game.py
@@ -8,7 +8,7 @@ screen = pygame.display.set_mode((400, 400))
 pygame.display.set_caption("Chomp!")
 screen.fill((100,100,150))
 pygame.draw.rect(screen, (100,25,0), (0, 380, 400, 400))
-pygame.draw.rect(screen, (100,25,0), (0, 200, 200, 200))
+pygame.draw.rect(screen, (0,150,0), (250, 50, 50, 50))
 pygame.display.flip()
 
 while True:
