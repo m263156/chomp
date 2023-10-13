03cc42592297cd9e253ee04c2df3a3779887df14
Kerry Johnson <m263156@usna.edu>
2023-10-13 09:45:16 -0400

    added sand

--- game.py
+++ game.py
@@ -8,6 +8,8 @@ pygame.display.set_caption("Chomp!")
 screen.fill((100,100,150))
 pygame.draw.rect(screen, (100,25,0), (0, 380, 400, 400))
 pygame.draw.rect(screen, (0,150,0), (250, 50, 50, 50))
+sand = pygame.image.load("assets/images/sand.png").convert()
+screen.blit(sand, (200,200, 64, 64))
 pygame.display.flip()
 
 while True:
