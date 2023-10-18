0e60ae47f11fa11bd64e9c16f65c55b30fd0b5ae
Kerry Johnson <m263156@usna.edu>
2023-10-18 09:26:36 -0400

    No Magic Numbers

--- game.py
+++ game.py
@@ -1,15 +1,24 @@
 import pygame
 import sys
 
+SCREEN_WIDTH = 1280
+SCREEN_HEIGHT = 720
+SAND_HEIGHT = 20
+TILE_SIZE = 64
+WATER_COLOR = (114, 159, 232)
+SAND_COLOR = (100, 25, 0)
 pygame.init()
 
-screen = pygame.display.set_mode((400, 400))
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Chomp!")
-screen.fill((100,100,150))
-pygame.draw.rect(screen, (100,25,0), (0, 380, 400, 400))
-pygame.draw.rect(screen, (0,150,0), (250, 50, 50, 50))
+screen.fill(WATER_COLOR)
+
+pygame.draw.rect(screen,
+                 SAND_COLOR,
+                 (0, SCREEN_HEIGHT-SAND_HEIGHT, SCREEN_WIDTH, SAND_HEIGHT))
 sand = pygame.image.load("assets/images/sand.png").convert()
-screen.blit(sand, (200,200, 64, 64))
+screen.blit(sand, (SCREEN_WIDTH/2 - TILE_SIZE/2, SCREEN_HEIGHT/2 - TILE_SIZE/2))
+
 pygame.display.flip()
 
 while True:
