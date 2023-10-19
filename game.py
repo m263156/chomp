b8e16d8c6e35f599773c60e4650158c2df09f9d7
Kerry Johnson <m263156@usna.edu>
2023-10-19 08:53:15 -0400

    added settings.py and created title screen

--- game.py
+++ game.py
@@ -1,27 +1,39 @@
 import pygame
 import sys
+import random
+from settings import *
 
-TILE_SIZE = 64
-SCREEN_WIDTH = 16 * TILE_SIZE
-SCREEN_HEIGHT = 9 * TILE_SIZE
-SAND_HEIGHT = 20
-WATER_COLOR = (114, 159, 232)
-SAND_COLOR = (135, 128, 107)
 pygame.init()
 
+game_font = pygame.font.Font("assets/fonts/Black_Crayon.ttf", TEXT_SIZE)
+# Builds Screen
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Chomp!")
 screen.fill(WATER_COLOR)
-
+# Sand Rectangle
 pygame.draw.rect(screen,
                  SAND_COLOR,
                  (0, SCREEN_HEIGHT-SAND_HEIGHT, SCREEN_WIDTH, SAND_HEIGHT))
+# Assets
 sand = pygame.image.load("assets/images/sand.png").convert()
 sand_top = pygame.image.load("assets/images/sand_top.png").convert()
+sand_top.set_colorkey((0, 0, 0))
+seagrass = pygame.image.load("assets/images/seagrass.png").convert()
+seagrass.set_colorkey((0, 0, 0))
+# Builds Bottom Sand
 for i in range(SCREEN_WIDTH//TILE_SIZE):
     screen.blit(sand, (i * TILE_SIZE, SCREEN_HEIGHT - TILE_SIZE))
+# Builds Top Sand
 for i in range(SCREEN_WIDTH//TILE_SIZE):
     screen.blit(sand_top, (i * TILE_SIZE, SCREEN_HEIGHT - (2*TILE_SIZE)))
+# Places 4 Seaweeds Randomly
+for i in range(4):
+    x = random.randint(0, SCREEN_WIDTH - (2*TILE_SIZE))
+    y = random.randint(SCREEN_HEIGHT - 2.5 * TILE_SIZE, SCREEN_HEIGHT - 0.5 * TILE_SIZE)
+    screen.blit(seagrass, (x, y))
+# Add Text
+title = game_font.render("Chomp!", True, (255, 0, 0))
+screen.blit(title, (SCREEN_WIDTH//2 - title.get_width()//2, SCREEN_HEIGHT//2 - title.get_height()//2))
 pygame.display.flip()
 
 while True:
